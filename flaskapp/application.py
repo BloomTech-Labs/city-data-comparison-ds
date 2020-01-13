a8fb689b2f9c4b3a32c8bb16be2cfa4d6fb9ca7d
scottwmwork <scottwmwork@users.noreply.github.com>
2020-01-13 16:09:50 -0700

    added dynamic endpoint and querydata() function

--- flaskapp/application.py
+++ flaskapp/application.py
@@ -1,9 +1,34 @@
 from flask import Flask
+from pymongo import MongoClient
 application = app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return 'test'
+
+
+# Connect to MongoDB
+client =  MongoClient("")
+db = client['city-data']
+collection = db['Census-Data']
+
+
+def querydata(city_state):
+
+    state = city_state.split('-')[0]
+    city = city_state.split('-')[1]
+    myquery = {"State": state} #TODO add query by state & city 
+    docs = collection.find(myquery)
+    return docs
+
+
+# Dynamic End Point
+@app.route("/<city_state>")
+def hello(city_state):
+    
+    # TODO Make calls to mongoDB to retrieve city data
+
+    # TODO return city data
+    return(city_state)
 
 if __name__ == "__main__":
     app.run()
+
+
