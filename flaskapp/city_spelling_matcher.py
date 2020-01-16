1961d89727743a02dddcd2f227952b17317c0276
matthew-sessions <matthew-sessions@users.noreply.github.com>
2020-01-16 07:40:45 -0800

    updated the city name mapper end point for better results when search for misspelled cities

--- flaskapp/city_spelling_matcher.py
+++ flaskapp/city_spelling_matcher.py
@@ -1,20 +1,33 @@
 import difflib
 import json
 
-def spellchecker(words):
-    with open('data/spellcheck/spell_check_opject.json', 'r') as myfile:
+def data_loader():
+    with open('data/spellcheck/spell_check_opject2.json', 'r') as myfile:
         data=myfile.read()
-    obj = json.loads(data)
+        obj = json.loads(data)
+    return(obj)
+
+
+def check_spelling(data, words):
     jsn = {}
-    lenth = []
-    for i in difflib.get_close_matches(words.lower(), list(obj.keys()), n=5):
-        lenth.append(i)
-        jsn[list(obj[i].keys())[0]] = list(obj[i].values())[0]
-    if len(lenth) > 0:
+    id_manager = []
+    for i in difflib.get_close_matches(words.lower(), list(data.keys()), n=15):
+        if list(data[i].values())[0]['ID'] not in id_manager:
+            id_manager.append(list(data[i].values())[0]['ID'])
+            jsn[list(data[i].keys())[0]] = list(data[i].values())[0]
+        else:
+            pass
+    if len(jsn) > 0 and len(jsn) <= 5:
         res = jsn
+    elif len(jsn) > 5:
+        short_dict = {}
+        for i in list(jsn.keys())[0:5]:
+            short_dict[i] = jsn[i]
+        res = short_dict
+
     else:
         if len(words.split()) <= 1:
-            res = {'No Data': f'Cannot find {words}, please include the State name along with the City you are searching for.'}
+            res = {'No Data': f'Cannot find {words}, please include the Ctate name along with the City you are searching for.'}
         else:
             res = {'No Data': f'Cannot find {words}, please check the spelling or search for another City.'}
     return(res)
