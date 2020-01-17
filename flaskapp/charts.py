89d3adf21f9cfbb925b3faa3897333a518b53f57
matthew-sessions <matthew-sessions@users.noreply.github.com>
2020-01-17 11:19:33 -0800

    added all charts to docs template

--- flaskapp/charts.py
+++ flaskapp/charts.py
@@ -214,7 +214,175 @@ def housingtest(seattle):
     fig8.update_yaxes(title_text='% of all homes')
 
 
-    graphs = [fig,fig2,fig3, fig4, fig5, fig6, fig7, fig8]
+    mothers = seattle['Recent Mothers']['Age Distribution']
+    motherna = seattle['Recent Mothers']['National Avg']
+    motherskeys = list(mothers.keys())
+    mothersvalues = list(mothers.values())
+    mothernakeys = list(motherna.keys())
+    mothernavalues = list(motherna.values())
+
+    fig9 = go.Figure(go.Pie(labels=motherskeys , values=mothersvalues, name="Mothers in Seattle"))
+
+    fig9.update_layout(
+        title= "Age Distribution of mothers who gave birth in the last 12 months",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig9.update_yaxes(title_text='% of all homes')
+
+
+    school = seattle['School Enrollment']
+    schoolkeys = list(school.keys())
+    schoolvalues = list(school.values())
+
+    fig10 = go.Figure(go.Bar(
+            x=schoolkeys,
+            y=schoolvalues,
+            name='School Enrollment'))
+
+    fig10.update_layout(
+        title= "Percentage distribution of school enrollment in Seattle",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig10.update_yaxes(title_text='% of population over age of 5')
+
+    education = seattle['Educational Attainment']
+    educationkeys = list(education.keys())
+    educationvalues = list(education.values())
+
+    fig11 = go.Figure(go.Bar(
+            x=educationkeys,
+            y=educationvalues,
+            name='Educational Attainment'))
+
+    fig11.update_layout(
+        title= "Percentage distribution of Educational Attainment",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig11.update_yaxes(title_text='% of population over age of 24')
+
+    ethnicity = seattle['Ethnicity']
+    ethnicitykeys = list(ethnicity.keys())
+    ethnicityvalues = list(ethnicity.values())
+
+    fig12 = go.Figure(go.Bar(
+            x=ethnicitykeys,
+            y=ethnicityvalues,
+            name='Ethnicity'))
+
+    fig12.update_layout(
+        title= "Percentage of population by Ethnicity",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig12.update_yaxes(title_text='% of population')
+
+
+
+    sex = seattle['Sex of Labor Force']
+    sexkeys = list(sex.keys())
+    sexvalues = list(sex.values())
+
+    fig13 = go.Figure(go.Bar(
+            x=sexkeys,
+            y=sexvalues,
+            name='Gender'))
+
+    fig13.update_layout(
+        title= "Percentage of work force by gender",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig13.update_yaxes(title_text='% of work force')
+
+    commute = seattle['Commuting to Work']
+    commutekeys = list(commute.keys())
+    commutevalues = list(commute.values())
+
+    fig14 = go.Figure(go.Bar(
+            x=commutekeys,
+            y=commutevalues,
+            name='Commute'))
+
+    fig14.update_layout(
+        title= "Percentage of work force by means of commute",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig14.update_yaxes(title_text='% of work force')
+
+
+
+    industry = seattle['Industry']
+    industrykeys = list(industry.keys())
+    industryvalues = list(industry.values())
+
+    fig15 = go.Figure(go.Bar(
+            x=industrykeys,
+            y=industryvalues,
+            name='Industry'))
+    fig15.update_layout(
+        title= "Percentage of work force by industry",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig15.update_yaxes(title_text='% of work force')
+
+    classofw = seattle['Class of Worker']
+    classofwkeys = list(classofw.keys())
+    classofwvalues = list(classofw.values())
+
+    fig16 = go.Figure(go.Bar(
+            x=classofwkeys,
+            y=classofwvalues,
+            name='Class of Worker'))
+    fig16.update_layout(
+        title= "Percentage of work force by class of worker",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig16.update_yaxes(title_text='% of work force')
+
+    occupation = seattle['Occupation']
+    occupationkeys = list(occupation.keys())
+    occupationvalues = list(occupation.values())
+
+    fig17 = go.Figure(go.Bar(
+            x=occupationkeys,
+            y=occupationvalues,
+            name='Occupation'))
+    fig17.update_layout(
+        title= "Percentage of work force by Occupation",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig17.update_yaxes(title_text='% of work force')
+
+    incomehouse = seattle['Household Income']
+    incomehousekeys = list(incomehouse.keys())
+    incomehousevalues = list(incomehouse.values())
+
+    fig18 = go.Figure(go.Bar(
+            x=incomehousekeys,
+            y=incomehousevalues,
+            name='Household Income'))
+    fig18.update_layout(
+        title= "Percentage of households by income",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig18.update_yaxes(title_text='% of households')
+
+    pci = seattle['Per Capita by Gender']
+    pcikeys = list(pci.keys())
+    pcivalues = list(pci.values())
+
+    fig19 = go.Figure(go.Bar(
+            x=pcikeys,
+            y=pcivalues,
+            name='Per capita income'))
+    fig19.update_layout(
+        title= "Per capita income by gender",
+        plot_bgcolor='#F9F9FB'
+    )
+    fig19.update_yaxes(title_text='Yearly Income in USD')
+
+
+
+
+    graphs = [fig,fig2,fig3, fig4, fig5, fig6, fig7, fig8, fig9, fig10, fig11, fig12, fig13, fig14, fig15, fig16, fig17, fig18, fig19]
 
     ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
     content = [['Housing Prices', 'Data pulled from the "Historical Property Value Data" key.', 'housing'],
@@ -224,7 +392,18 @@ def housingtest(seattle):
                 ['Selected Monthly Owner Costs with Mortgage','',''],
                 ['Vehicles Available by Home','',''],
                 ['Population Growth','','social'],
-                ['Marital Status by Gender','','']]
+                ['Marital Status by Gender','',''],
+                ['Recent Mothers','',''],
+                ['School Enrollment','',''],
+                ['Educational Attainment','',''],
+                ['Ethnicity','',''],
+                ['Work Force by Gender','','work'],
+                ['Method of Commuting to Work','',''],
+                ['Industry','',''],
+                ['Class of worker','',''],
+                ['Occupation','',''],
+                ['Household Income','','eco'],
+                ['Per Capita Income','','']]
 
     # Convert the figures to JSON
     # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
